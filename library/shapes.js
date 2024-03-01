4f0d4d9cb48977c35b77afbea918ea023922e44d
David Wright <davidwright7793@gmail.com>
2024-03-01 18:54:54 -0500

    added square and triangle classes

--- library/shapes.js
+++ library/shapes.js
@@ -13,3 +13,33 @@ render (){
 }
 
 // new Circle().setColor("burgundy").render()  We create new circle instance, set color to burgundy, and then render tag with burgundy fill
+
+class Square{
+    constructor(){
+        this.color = ""
+    }
+    
+    setColor(chosenColor){
+        this.color = chosenColor
+    }
+    
+    render (){
+        return `<rect width="120" height="120" x="10" y="10" fill="${this.color}" />`
+    }
+}
+
+class Triangle{
+    constructor(){
+        this.color = ""
+    }
+    
+    setColor(chosenColor){
+        this.color = chosenColor
+    }
+    
+    render (){
+        return `<polygon points="150, 18 244, 182 56, 182" fill="${this.color}" />`
+    }
+}
+
+module.exports = {Circle, Square, Triangle}
